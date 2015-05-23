21b9e70186a77f41fe0157f0dfa3be8541cad646
kennedyal <allen.kennedy@student.dit.ie>
2015-05-23 14:54:10 +0100

    Coursera R Programming Assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+#create function that stores functions that allows them to be called
+makeCacheMatrix <- function(x = matrix()) #create function of functions
+	{	##initial cache is null
+		c <- NULL 
+		##pass y into x in main function above
+		setMatrix <- function(y) 
+			{
+				x<<- y
+				#sets c to be null
+				c <<- NULL
+			}
+			#return x matrix
+		getMatrix <- function() x 
+		#cache the inverse value
+		setInverse <- function(inverse) 
+			{	
+				c <<- inverse
+			}
+			#return the cached inversed value
+		getInverse <- function() c 
+		#return a list of the values
+		list(setMatrix = setMatrix, getMatrix = getMatrix, setInverse = setInverse, getInverse = getInverse)
+	}
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+#next function calculates the inverse of a given matrix
+#takes matrix as input parameter into function
+cacheSolve <- function(x = matrix(),...)
+	{	#calls getInverse function which returns c and will pass into m
+		m <- x$getInverse()
+		#checks if inverse already exists
+		if (!is.null(m))
+		{	#returns this message if it is not null
+			message('getting cached data')
+			#then returns m
+			m
+		}
+		#get new matrix and passes into data
+		data <-x$getMatrix()
+		#calculates the inverse of data and passes into m
+		m <- solve(data)
+		#calls set inverse function which stores m into c
+		x$setInverse(m)
+		#prints m
+		m
+	}
